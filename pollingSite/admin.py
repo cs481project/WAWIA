84d5b41fbf820c1d974af21903c587429c89225c
Kyle Discher <kadisch96@gmail.com>
2018-01-15 16:10:21 -0800

    Added Models

--- pollingSite/admin.py
+++ pollingSite/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 
-from .models import Poll
+from .models import *
 
-admin.site.register(Poll)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'studentID')
+
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ('className', 'classNumber', 'instructor')
+
+class PollAdmin(admin.ModelAdmin):
+    readonly_fields=('key',)
+    list_display = ('name', 'classroom', 'startTime', 'stopTime')
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('poll', 'student', 'timestamp')
+
+
+admin.site.register(Instructor, InstructorAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Classroom, ClassroomAdmin)
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- pollingSite/models.py
+++ pollingSite/models.py
@@ -1,7 +1,52 @@
+from string import ascii_uppercase, digits
+from random import choices
 from django.db import models
+
+class Instructor(models.Model):
+    name = models.CharField(max_length = 128, unique=True)
+    def __str__(self):
+        return self.name
+
+class Classroom(models.Model):
+    classNumber = models.CharField(max_length = 20, unique=True)
+    className = models.CharField(max_length = 64, default=classNumber)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.className
+
+class Student(models.Model):
+    name = models.CharField(max_length = 128)
+    studentID = models.IntegerField(unique=True)
+    classrooms = models.ManyToManyField(Classroom)
+    def __str__(self):
+        return self.name
 
 class Poll(models.Model):
     name = models.CharField(max_length = 64)
     options = models.IntegerField()
+    key = models.CharField(max_length = 6, unique=True, default='')
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, null=True)
+    startTime = models.DateTimeField(null=True)
+    stopTime = models.DateTimeField(null=True)
+    def __init__(self, *args, **kwargs):
+        super(Poll, self).__init__(*args, **kwargs)
+        chars = ascii_uppercase + digits
+        self.key = ''.join(choices(chars, k=5))
+
     def __str__(self):
         return self.name
+
+    def isActive(self):
+        if(self.startTime < datetime.now()):
+            if(self.stopTime is not null or (self.stopTime > datetime.now())):
+                return True
+        return False
+
+class Answer(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    value = models.IntegerField()
+    timestamp = models.DateTimeField()
+    def __str__(self):
+        return '{} + {}'.format(self.student, self.poll)
