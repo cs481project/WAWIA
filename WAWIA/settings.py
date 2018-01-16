eac71a0323c247ff0f269fa9894a39a2d8f59bec
keylacerna <33634397+keylacerna@users.noreply.github.com>
2018-01-15 21:36:30 -0800

    Update settings.py

--- WAWIA/settings.py
+++ WAWIA/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'WAWIA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstoneproject',
+        'USER': '404GroupMemberNotFound',
+        'PASSWORD': 'kckdjsag2018',
+        'HOST': 'localhost':
+        'PORT': '',
     }
 }
 
