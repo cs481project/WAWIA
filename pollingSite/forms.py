a7fa30be78cbde4a3533684fbed74dd235be6eaa
Kyle Discher <kadisch96@gmail.com>
2018-03-08 18:30:21 -0800

    The great purge

--- pollingSite/forms.py
+++ pollingSite/forms.py
@@ -23,11 +23,6 @@ class editClassForm(forms.ModelForm):
     start_time = forms.TimeField(label="Start Time (24hr)", widget=forms.TimeInput({"placeholder":"HH:MM"}, format='%H:%M'))
     end_time = forms.TimeField(label="Start Time (24hr)", widget=forms.TimeInput({"placeholder":"HH:MM"}, format='%H:%M'))
 
-class copyClassForm(forms.Form):
-    class_name = forms.CharField(max_length=50)
-    quarter = forms.ChoiceField(choices=SEASONS)
-    year = forms.ChoiceField(choices=[(i,i) for i in range(2018, datetime.now().year + 2)])
-
 class createPollForm(forms.Form):
     choose_class = forms.ModelChoiceField(queryset=Classroom.objects.annotate(class_count=Count('className')))
     possible_answers = forms.ChoiceField(choices=[(i,i) for i in range(2,13)], label="# of options for this poll")
@@ -39,19 +34,6 @@ class correctAnswerForm(forms.Form):
         if 'choices' in kwargs:
             choices = kwargs.pop('choices')
             self.fields['correct_answer'].choices=[(i,chr(64+i)) for i in range(1,choices+1)]
-
-class attendanceFormForm(forms.Form):
-    start_date = forms.DateField(widget=forms.SelectDateWidget)
-    end_date = forms.DateField(widget=forms.SelectDateWidget)
-
-class activePollForm(forms.Form):
-    start_time = forms.DateTimeField()
-    end_time = forms.DateTimeField()
-    
-class settingForm(forms.Form):
-    email = forms.CharField(max_length=50, required=False)
-    first_name = forms.CharField(max_length=50, required=False)
-    last_name = forms.CharField(max_length=50, required=False)
 
 class reportForm(forms.Form):
     choose_class = forms.ModelChoiceField(queryset=Classroom.objects.annotate(class_count=Count('className')))
